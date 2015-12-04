a521515136fcb39e8011966470e2990f4bebe6b1
Stephen Becker <stephen.beckr@gmail.com>
2015-12-03 17:10:54 -0700

    Improve code for adding paths

--- setup_kmeans.m
+++ setup_kmeans.m
@@ -9,7 +9,7 @@ function setup_kmeans
 %% Setup path
 
 baseDirectory = fileparts(mfilename('fullpath'));
-addpath(genpath(baseDirectory));
+addpath(genpath_ignoreHiddenDir(baseDirectory));
 
 %% Compile the KMeans sparse-matrix-assignmenet code
 if exist('SparseMatrixMinusCluster','file')~=3
@@ -41,4 +41,41 @@ if exist('hadamard','file')~=3
         mex('-O',threaddFlg,'-UDEBUG','-DNO_UCHAR','hadamard_pthreads.c');
     end
     cd(here);
+end
+
+
+function p = genpath_ignoreHiddenDir(d)
+%%
+% initialise variables
+classsep = '@';  % qualifier for overloaded class directories
+packagesep = '+';  % qualifier for overloaded package directories
+p = '';           % path to be returned
+
+% Generate path based on given root directory
+files = dir(d);
+if isempty(files)
+  return
+end
+
+% Add d to the path even if it is empty.
+p = [p d pathsep];
+
+% set logical vector for subdirectory entries in d
+isdir = logical(cat(1,files.isdir));
+%
+% Recursively descend through directories which are neither
+% private nor "class" directories.
+%
+dirs = files(isdir); % select only directory entries from the current listing
+
+for i=1:length(dirs)
+   dirname = dirs(i).name;
+   if    ~strcmp( dirname,'.')          && ...
+         ~strcmp( dirname,'..')         && ...
+         ~strncmp( dirname,classsep,1) && ...
+         ~strncmp( dirname,packagesep,1) && ...
+         ~strcmp( dirname,'private') && ...
+         ~strcmpi( dirname(1), '.' ) % added in order to exclude .git/ files
+      p = [p genpath(fullfile(d,dirname))]; % recursive calling of this function.
+   end
 end
